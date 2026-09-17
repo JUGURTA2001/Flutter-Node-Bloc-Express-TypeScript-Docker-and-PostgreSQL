eac980dd2851828b05e5741b4745835473405a6a
jugurta2001 <jugurta.touati@se.univ-bejaia.dz>
2026-09-17 17:08:03 +0200

    fluuetr et backen

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,7 +1,8 @@
-import { Pool } from 'pg';
-import {drizzle} from 'drizzle-orm/node-postgres';
-const  pool=new Pool({
-    connectionString:"postgresql://postgres:Mysql@123@db:5432/mydb",
-})
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 
-export const   db = drizzle(pool)
+const pool = new Pool({
+  connectionString: "postgresql://postgres:Mysql@123@db:5432/mydb",
+});
+
+export const db = drizzle(pool);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,17 @@
-import express from 'express';
-import authRouter from './routes/auth';   // ← changement ici
+import express from "express";
+import authRouter from "./routes/auth";
+import taskRouter from "./routes/task";
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/auth", authRouter)
+app.use("/auth", authRouter);
+app.use("/tasks", taskRouter);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to my app !!!!!t!!!")
-})
+  res.send("Welcome to my app!!!!!!!");
+});
 
 app.listen(8000, () => {
-    console.log("server started on port 8000");  // ← petite correction au passage
-})
+  console.log("Server started on port 8000");
+});
